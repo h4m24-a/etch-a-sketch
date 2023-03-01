b8a9224ae61c1c52a909b2f4dc6094b646f0715d
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-03-01 19:22:55 +0000

    add function to clear grid

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-let container = document.querySelector('.container');
+const containerDiv = document.querySelector('.container');
 
 // Add 16 divs
 for (let i = 1; i < (257+16); i++) {
   let div = document.createElement('div');
-  container.appendChild(div).className='smallSquares';
+  containerDiv.appendChild(div).className='smallSquares';
   div.addEventListener('mouseover', () => {
     div.style.backgroundColor = 'black';
   })
 }
+
+
+// Reset Button
+function clearGrid() {
+  containerDiv.childNodes.forEach((div) =>
+    div.style.backgroundColor = "white"
+  )}

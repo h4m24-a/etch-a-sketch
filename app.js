00f792d5b0b251fb922324d1601e8168f55e9351
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-03-02 19:22:14 +0000

    refactor code

--- app.js
+++ app.js
@@ -1,49 +1,35 @@
 const containerDiv = document.querySelector('.container');
 
+// Buttons
+const selectBtn = document.getElementById('selectBtn')
+const eraserBtn = document.getElementById('eraserBtn')
+const rainbowBtn = document.getElementById('rainbowBtn')
+const clearBtn = document.getElementById('clearBtn')
+const colourBtn = document.getElementById('colourBtn')
+
 createGrid();
-// Add 16 divs
+
 function createGrid () {
-for (let i = 1; i < (257+16); i++) {
-  let div = document.createElement('div');
-  containerDiv.appendChild(div).className='smallSquares';
-  div.addEventListener('mouseover', () => {
-    div.style.backgroundColor = 'black';
-  })
-  }
+
 };
 
 // Resize Grid Button
-function selectGrid() {
+
   
-}
 
 
 
 // Reset Button
-function clearGrid() {
-
-  }
 
 
 // Erasor Button
-function eraserColour() {
 
-}
 
 // Colour Button
-function colourSelector() {
-  
-}
+
+
+
 // Rainbow Colour button
-function rainbowColour() {
-  
-}
 
 
 
-// Buttons
-  selectBtn.addEventListener('click', selectGrid)
-  clearBtn.addEventListener('click', clearGrid);
-  rainbowBtn.addEventListener('click', rainbowColour);
-  colourBtn.addEventListener('click', colourSelector);
-  eraserBtn.addEventListener('click', eraserColour);

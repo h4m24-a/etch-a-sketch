ae8b3ddded08e1437fc451655d7f14a0bf29b416
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-03-02 17:20:43 +0000

    add event listener to buttons

--- app.js
+++ app.js
@@ -1,17 +1,49 @@
 const containerDiv = document.querySelector('.container');
 
+createGrid();
 // Add 16 divs
+function createGrid () {
 for (let i = 1; i < (257+16); i++) {
   let div = document.createElement('div');
   containerDiv.appendChild(div).className='smallSquares';
   div.addEventListener('mouseover', () => {
     div.style.backgroundColor = 'black';
   })
+  }
+};
+
+// Resize Grid Button
+function selectGrid() {
+  
 }
+
 
 
 // Reset Button
 function clearGrid() {
-  containerDiv.childNodes.forEach((div) =>
-    div.style.backgroundColor = "white"
-  )}
+
+  }
+
+
+// Erasor Button
+function eraserColour() {
+
+}
+
+// Colour Button
+function colourSelector() {
+  
+}
+// Rainbow Colour button
+function rainbowColour() {
+  
+}
+
+
+
+// Buttons
+  selectBtn.addEventListener('click', selectGrid)
+  clearBtn.addEventListener('click', clearGrid);
+  rainbowBtn.addEventListener('click', rainbowColour);
+  colourBtn.addEventListener('click', colourSelector);
+  eraserBtn.addEventListener('click', eraserColour);

08b8e0bf20eb6e96e839c85bddc64bbddd7b0a9b
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-03-03 00:44:47 +0000

    refactored coding for creating grid, updating grid and fixed functions to allow you to change color for erasor and rgb

--- app.js
+++ app.js
@@ -1,35 +1,73 @@
-const containerDiv = document.querySelector('.container');
+const container = document.querySelector(".container");
+const userInput = document.getElementById("quantity");
+let color = "black";
 
 // Buttons
-const selectBtn = document.getElementById('select')
-const eraserBtn = document.getElementById('eraser')
-const rainbowBtn = document.getElementById('rainbow')
-const clearBtn = document.getElementById('clear')
-const colourBtn = document.getElementById('colour')
+const selectBtn = document.getElementById("selectGrid");
+const clearBtn = document.getElementById("clear");
+const rainbowBtn = document.getElementById("rainbow");
+const colourBtn = document.getElementById("colour");
+const eraserBtn = document.getElementById("eraser");
+
+
+createGrid = () => {
+  for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.classList.add("smallsquares");
+    container.appendChild(div);
+    div.addEventListener('mouseover', colorSquare);
+      div.style.backgroundColor = '';
+    }
+  }
+
+
+updateGrid = () => {
+  container.innerHTML = "";
+  container.style.setProperty(
+    "grid-template-columns",
+    `repeat(${userInput.value}, 2fr)`
+  );
+  container.style.setProperty(
+    "grid-template-rows",
+    `repeat(${userInput.value}, 2fr)`
+  );
+  for (let i = 0; i < userInput.value * userInput.value; i++) {
+    const div = document.createElement("div");
+    div.classList.add("smallsquares");
+    container.appendChild(div);
+    div.addEventListener('mouseover', colorSquare);
+      div.style.backgroundColor = '';
+    }
+  };
+
+
+// Colours
+function colorSquare () {
+  if (color === 'rainbow'){
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+  this.style.backgroundColor = color;
+  }
+};
+
+function changeColor(choice){
+  color = choice
+};
+
+
+
+userInput.addEventListener("change", updateGrid);
 
 createGrid();
 
-function createGrid () {
-
-};
-
-// Resize Grid Button
-
-  
 
 
 
-// Reset Button
-
-
-// Erasor Button
-
-
-// Colour Button
-
-
-
-// Rainbow Colour button
-
-
-
+// Clear Button
+clearBtn.addEventListener("click", function () {
+  container.innerHTML = "";
+  userInput.value = "";
+  container.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
+  container.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
+  createGrid();
+});

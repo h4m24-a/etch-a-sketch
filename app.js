bf201a1ac98007c529addd27a8f500dc751f444a
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-03-04 00:18:09 +0000

    refactor code and fixed the function for the color picker

--- app.js
+++ app.js
@@ -5,21 +5,17 @@ let color = "black";
 // Buttons
 const selectBtn = document.getElementById("selectGrid");
 const clearBtn = document.getElementById("clear");
-const rainbowBtn = document.getElementById("rainbow");
-const colourBtn = document.getElementById("colour");
-const eraserBtn = document.getElementById("eraser");
-
+const screenVal = document.querySelector(".value");
 
 createGrid = () => {
   for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
     div.classList.add("smallSquares");
     container.appendChild(div);
-    div.addEventListener('mouseover', colorSquare);
-      div.style.backgroundColor = '';
-    }
+    div.addEventListener("mouseover", colorSquare);
+    div.style.backgroundColor = "";
   }
-
+};
 
 updateGrid = () => {
   container.innerHTML = "";
@@ -35,55 +31,39 @@ updateGrid = () => {
     const div = document.createElement("div");
     div.classList.add("smallsquares");
     container.appendChild(div);
-    div.addEventListener('mouseover', colorSquare);
-      div.style.backgroundColor = '';
-    }
-  };
-
-
-// Colours
-function colorSquare () {
-  if (color === 'rainbow'){
-    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-  } else {
-  this.style.backgroundColor = color;
+    div.addEventListener("mouseover", colorSquare);
+    div.style.backgroundColor = "";
   }
 };
 
-function changeColor(choice){
-  color = choice
-};
+// Colours
+function colorSquare() {
+  if (color === "color") {
+    let newColor = document.getElementById("color").value;
+    this.style.backgroundColor = newColor;
+  } else if (color === "rainbow") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = color;
+  }
+}
 
+function changeColor(choice) {
+  color = choice;
+}
 
 // Slider for Grid
-const screenVal = document.querySelector('.value');
-quantity.addEventListener('input', function(){
-    let val = userInput.value;
-    screenVal.textContent = val;
-})
-
+quantity.addEventListener("input", function () {
+  let val = userInput.value;
+  screenVal.textContent = val;
+});
 
 userInput.addEventListener("change", updateGrid);
 
-createGrid();
-
-
-
-
 // Clear Button
-clearBtn.addEventListener('click', clearGrid);
+clearBtn.addEventListener("click", clearGrid);
 function clearGrid() {
-  container.childNodes.forEach((div) =>
-    div.style.backgroundColor = "white"
-  )}
+  container.childNodes.forEach((div) => (div.style.backgroundColor = "white"));
+}
 
-// clearBtn.addEventListener("click", function () {
-//   container.innerHTML = "";
-//   userInput.value = "16";
-//   screenVal.innerHTML = '16';
-//   container.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
-//   container.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
-//   createGrid();
-// });
-
-
+createGrid();
